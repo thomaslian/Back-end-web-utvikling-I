4e2d41112acec4324a4f50efea93e4828aa684c2
Thomas Oedegaard <thomaslio@live.no>
2020-09-04 12:40:49 +0200

    Changed form prev lab

--- lab02/Lab2Ex1.php
+++ lab02/Lab2Ex1.php
@@ -8,15 +8,14 @@
 <body>
 
     <?php
+    $username = 'Thomas';
+    $numNotifications = 8;
+    $numEmails = 2;
+    $notificationText = "You have $numNotifications notifications";
+    ?>
 
-$username = "Thomas";
-$numNotifications = 8;
-$numEmails = 2;
-$notificationText = "You have " . $numNotifications . " notifications";
-
-?>
-    <h1><?php echo "Welcome" . $username . "!"; ?></h1>
-    <h2><?php echo $notificationText . " and " . $numEmails . " emails!"; ?></h2>
+    <h1><?php echo 'Welcome' . $username . '!'; ?></h1>
+    <h2><?php echo "$notificationText and $numEmails emails!"; ?></h2>
 
 </body>
 
