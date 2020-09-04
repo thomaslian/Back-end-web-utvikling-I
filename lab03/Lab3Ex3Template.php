4a369f88bbcd392096fc04428ef828b6519ec8ff
Thomas Oedegaard <thomaslio@live.no>
2020-09-04 12:23:51 +0200

    Working on Ex3 Lab3

--- lab03/Lab3Ex3Template.php
+++ lab03/Lab3Ex3Template.php
@@ -7,35 +7,45 @@
 <body>
     
     <?php
-    
     // Algorithm for bubble sort
+    function bubbleSort($input)
+    {
+        $temp[] = $input;
+        $n = count($temp);
+        $flag = 0;
 
-    function bubbleSort($input){
-        $temp = $input;
-        
+        $x = 0; // i
+        $y = 0; // j
         // implement bubblesort algorithm here, use while or do..while loops and/or break and continue statements
+        while ($x < $n) {
+            while ($y < $n - $x - 1) {
+                if ($input[$y] > $input[$y + 1]) {
+                    // Swap elements
+                    $temp[] = $input[$y];
+                    $input[$y] = $input[$y + 1];
+                    $input[$y + 1] = $temp;
+                    $flag = 1;
+                }
+                $y++;
+            }
+            $x++;
+            if ($flag == 0) {
+                break;
+            }
+        }
 
-        
-        
         // temp should be the sorted array
         return $temp;
-        
-
     }
-    
-      
-    // test your function with different input.   
-    $inputArray = [4,5,8,2,0,1,10];
-    print_r($inputArray);
-    echo "<br>";
-    
-    $sortedArray = bubbleSort($inputArray);
-    echo "Sorted Array : ";
-    print_r($sortedArray);
-    
-       
 
-    
+    // test your function with different input.
+    $inputArray = [4, 5, 8, 2, 0, 1, 10];
+    print_r($inputArray);
+    echo '<br>';
+
+    $sortedArray = bubbleSort($inputArray);
+    echo 'Sorted Array : ';
+    print_r($sortedArray);
     ?>
    
 </body>
