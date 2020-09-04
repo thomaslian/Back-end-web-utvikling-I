bf8c657731c79f46fa32e15dcf2178c5c7cd9a09
Thomas Oedegaard <thomaslio@live.no>
2020-09-04 11:48:17 +0200

    Done with Ex2 Lab3

--- lab03/Lab3Ex2Template.php
+++ lab03/Lab3Ex2Template.php
@@ -5,29 +5,37 @@
     <title>Document</title>
 </head>
 <body>
-<h3>Chess Board using Nested For Loop</h3>
-   <table width="270px" cellspacing="0px" cellpadding="px" border="1px">
+    <h3>Chess Board using Nested For Loop</h3>
+    <table width="270px" cellspacing="0px" cellpadding="px" border="1px">
     
-    <?php
-       
-       // initialise first for loop for creating rows
+    <?php 
+    // initialise first for loop for creating rows
+    for ($x = 1; $x <= 8; $x++) {
+        // html tag for row
+        echo '<tr>';
+        // initialise second for loop for creating cols
+        for ($y = 1; $y <= 8; $y++) {
+            //creating condition for cell shading
+            $sum = $x + $y;
+            $color = '#FFFFFF';
 
-           
-           // html tag for row
-
-           // initialise second for loop for creating cols
-
-                //creating condition for cell shading
-       
-                //html tag for populating columns
-       
+            if ($sum % 2 == 0) {
+                // Even
+                $color = '#FFFFFF';
+            } else {
+                // Odd
+                $color = '#000000';
+            }
+            //html tag for populating columns
+            echo "<td height=30px width=30px bgcolor=$color></td>";
             //close second for loop
-
-           // close html tag for row
-
-       // close first for loop
+        }
+        // close html tag for row
+        echo '</tr>';
+        // close first for loop
+    } ?>
     
-    ?>
+</table>
    
 </body>
 </html>
